7654f22d55c807cfb4d6581433d35fb974ac8fe6
Tina2010 <t.rittmueller@gmx.de>
2021-12-29 16:01:01 +0100

    [feat] change of Username requirements

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }),(re
 
 // POST Allow new users to register
 app.post('/users',[
-  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username is required').not().isEmpty(),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to be valid').isEmail()
